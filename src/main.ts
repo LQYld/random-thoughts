74587a89fbe18bfe0030fc1b4628d2c6c6591e59
LQYld <54627537+LQYld@users.noreply.github.com>
2024-01-15 21:24:07 +0800

    feat: Remove useless code

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import '@unocss/reset/tailwind.css'
 import NProgress from 'nprogress'
 import dayjs from 'dayjs'
 import FloatingVue from 'floating-vue'
-import { setupRouterScroller } from 'vue-router-better-scroller'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat.js'
 import autoRoutes from 'pages-generated'
 import './styles/main.css'
@@ -26,32 +25,15 @@ export const createApp = ViteSSG(
   {
     routes,
   },
-  ({ router, app, isClient }) => {
+  ({ router, app }) => {
     dayjs.extend(LocalizedFormat)
 
     app.use(FloatingVue)
-
-    // if (isClient) {
-    //   const html = document.querySelector('html')!
-    //   setupRouterScroller(router, {
-    //     selectors: {
-    //       html(ctx) {
-    //         if (ctx.savedPosition?.top)
-    //           html.classList.add('no-sliding')
-    //         else
-    //           html.classList.remove('no-sliding')
-    //         return true
-    //       },
-    //     },
-    //     behavior: 'auto',
-    //   })
-
-    //   router.beforeEach((to,form,next) => {
-    //     NProgress.start()
-    //   })
-    //   router.afterEach(() => {
-    //     NProgress.done()
-    //   })
-    // }
+      router.beforeEach((to,form,next) => {
+        NProgress.start()
+      })
+      router.afterEach(() => {
+        NProgress.done()
+      })
   },
 )
