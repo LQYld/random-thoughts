fe51c7915da233535fa9fa11c606749ee2cf50a5
LQYld <54627537+LQYld@users.noreply.github.com>
2024-01-15 21:25:20 +0800

    feat: Try to fix known issues

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import '@unocss/reset/tailwind.css'
 import NProgress from 'nprogress'
 import dayjs from 'dayjs'
 import FloatingVue from 'floating-vue'
+import { setupRouterScroller } from 'vue-router-better-scroller'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat.js'
 import autoRoutes from 'pages-generated'
 import './styles/main.css'
@@ -25,15 +26,32 @@ export const createApp = ViteSSG(
   {
     routes,
   },
-  ({ router, app }) => {
+  ({ router, app, isClient }) => {
     dayjs.extend(LocalizedFormat)
 
     app.use(FloatingVue)
+
+    if (isClient) {
+      const html = document.querySelector('html')!
+      setupRouterScroller(router, {
+        selectors: {
+          html(ctx) {
+            if (ctx.savedPosition?.top)
+              html.classList.add('no-sliding')
+            else
+              html.classList.remove('no-sliding')
+            return true
+          },
+        },
+        behavior: 'auto',
+      })
+
       router.beforeEach((to,form,next) => {
         NProgress.start()
       })
       router.afterEach(() => {
         NProgress.done()
       })
+    }
   },
 )
