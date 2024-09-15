6a3ed8ce3471393e8fb9a6998aeed069ce3959df
LQYld <54627537+LQYld@users.noreply.github.com>
2024-09-15 20:25:05 +0800

    fix: footer text message

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="mt-10 mb-6 prose m-auto flex">
     <span class="text-sm op50"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> 2024-PRESENT ©   <ruby>
-    Qing
-    <rt>qìng</rt>
+    Ethan
   </ruby>
   <ruby>
-    yun
-    <rt>yǔn</rt>
-  </ruby>119</span>
+     Li
+  </ruby></span>
     <div class="flex-auto" />
   </div>
 </template>
